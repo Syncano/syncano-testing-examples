a61b5eb847bcb03bae6b4ad6c0df94a87aec5b06
Marcin Godlewski <gh3r@users.noreply.github.com>
2016-09-13 10:59:16 +0200

    Delete files for part one from root directory

--- globals.js
+++
@@ -1,3 +0,0 @@
-export default {
-  waitForConditionTimeout: 10000,
-};
--- pages/instancesPage.js
+++
@@ -1,8 +0,0 @@
-export default {
-  elements: {
-    instancesListDescription: {
-      selector: '//div[@class="description-field col-flex-1"]',
-      locateStrategy: 'xpath'
-    }
-  }
-};
